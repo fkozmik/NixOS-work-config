2569d3861e7e82d2a36358acebae035e1abe257c
fkozmik <florian.kozmik@enalean.com>
2025-06-12 10:07:28 +0200

    feat: Add secret scanning check as quick command

--- Users/aliases.nix
+++ Users/aliases.nix
@@ -6,7 +6,7 @@
     alias gl='git log --oneline'
     alias accesscreator="~/Toolbox/vault-tuleap-dynamic-creds-helper.phar"
     alias login="evlogin && vault token renew -increment=2h"
-    alias check="nix-shell --command 'make {editorconfig-checker,ansible-lint,tflint}'"
+    alias check="nix-shell --command 'make {editorconfig-checker,ansible-lint,tflint,scan-secrets}'"
 
     alias rebuild="sudo nixos-rebuild switch --impure"
 
