24a0b131669a7703293e343c93f662371c884320
fkozmik <florian.kozmik@enalean.com>
2025-06-30 16:30:01 +0200

    chore: Have Better battery mgmt

--- Users/battery-management.nix
+++ Users/battery-management.nix
@@ -10,8 +10,7 @@
                 CPU_BOOST_ON_BAT = 0;
                 CPU_SCALING_GOVERNOR_ON_AC = "performance";
                 CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-                START_CHARGE_THRESH_BAT0 = 55;
-                STOP_CHARGE_THRESH_BAT0 = 85;
+                STOP_CHARGE_THRESH_BAT0 = 95;
             };
         };
     system76-scheduler.settings.cfsProfiles.enable = true;
