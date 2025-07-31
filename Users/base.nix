0c5814d7e8f4550a2cb2342ee8f99aa5b78ffcbe
fkozmik <florian.kozmik@enalean.com>
2025-07-31 12:10:50 +0200

    feat: Configure OSQuery

--- Users/base.nix
+++ Users/base.nix
@@ -76,6 +76,29 @@
       settings.PasswordAuthentication = false;
       #settings.PermitRootLogin = "yes";
     };
+    osquery = {
+      enable = true;
+      flags = {
+        enroll_secret_path = "/home/fkozmik/.config/osquery/enroll_secret";
+        tls_hostname = "laptops.fleetdm.enalean.com:443";
+        tls_server_certs = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+        enroll_tls_endpoint = "/api/v1/osquery/enroll";
+        config_plugin = "tls";
+        config_tls_endpoint = "/api/v1/osquery/config";
+        config_refresh = "600";
+        disable_distributed = "false";
+        distributed_plugin = "tls";
+        distributed_tls_max_attempts = "10";
+        distributed_tls_read_endpoint = "/api/v1/osquery/distributed/read";
+        distributed_tls_write_endpoint = "/api/v1/osquery/distributed/write";
+        logger_plugin = "tls";
+        logger_tls_endpoint = "/api/v1/osquery/log";
+        host-identifier = "uuid";
+        enable_syslog = "false";
+        disable_events = "false";
+        enable_bpf_events = "true";
+      };
+    };
     pipewire = {
       enable = true;
       alsa.enable = true;
