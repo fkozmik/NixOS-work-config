09add177cfc3a1109ec1824a7967497e3b214394
fkozmik <florian.kozmik@enalean.com>
2025-10-27 12:15:36 +0100

    feat: Configure MXMaster 3s declaratively

--- roles/users.nix
+++ roles/users.nix
@@ -35,6 +35,7 @@
       glibc
       google-chrome
       libreoffice
+      logiops
       logseq
       nmon
       oh-my-zsh
@@ -54,4 +55,88 @@
       yubikey-manager-qt
     ]; 
   };
+ 
+  systemd.services.logiops = {
+    description = "Logitech Configuration Daemon";
+    startLimitIntervalSec = 0;
+    after = [ "multi-user.target" ];
+    wantedBy = [ "graphical.target" ];
+    wants = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.logiops}/bin/logid -v";
+      User = "root";
+    };
+  };
+
+  environment.etc."logid.cfg".text = ''
+  devices: (
+  {
+    name: "MX Master 3S";
+    
+    smartshift: {
+      on: true;
+      threshold: 15;
+    };
+    
+    hiresscroll: {
+      hires: true;
+      invert: true;
+      target: false; 
+    };
+    
+    dpi: 1000;
+    
+    buttons: (
+      {
+        cid: 0xc3;
+        action = {
+          type: "Gestures";
+          gestures: (
+            {
+              direction: "Up";
+              mode: "OnRelease";
+              action = {
+                type: "Keypress";
+                keys: ["KEY_LEFTMETA", "KEY_ESC"];
+              };
+            },
+            {
+              direction: "Down";
+              mode: "OnRelease";
+              action = {
+                type: "Keypress";
+                keys: ["KEY_LEFTMETA", "KEY_W"];
+              };
+            },
+            {
+              direction: "Left";
+              mode: "OnRelease";
+              action = {
+                type: "Keypress";
+                keys: ["KEY_LEFTMETA", "KEY_V"];
+              };
+            },
+            {
+              direction: "Right";
+              mode: "OnRelease";
+              action = {
+                type: "Keypress";
+                keys: ["KEY_LEFTMETA", "KEY_T"];
+              };
+            },
+            {
+              direction: "None";
+              mode: "OnRelease";
+              action = {
+                type: "Keypress";
+                keys: ["KEY_LEFTMETA"];
+              };
+            }
+          );
+        };
+      }
+    );
+  });
+  '';
 }
