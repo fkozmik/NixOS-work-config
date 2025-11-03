a9bf5de3ba6fccf976a43d2f8a923c27885fe541
fkozmik <florian.kozmik@enalean.com>
2025-11-03 17:02:28 +0100

    feat: update fonts

--- roles/fonts.nix
+++ roles/fonts.nix
@@ -4,12 +4,16 @@
     fontconfig = {
       defaultFonts = {
         monospace = [ "JetBrainsMono" ];
+        serif = [ "public-sans" ];
+        sansSerif = [ "public-sans" ];
       };
     };
     packages = with pkgs; [
+      dm-sans
       nerd-fonts.fira-code
       nerd-fonts.droid-sans-mono
       nerd-fonts.jetbrains-mono
+      public-sans
     ];
   };
 }
