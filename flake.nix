0bfc451c8285db58154d822982ee71907e530988
fkozmik <florian.kozmik@enalean.com>
2026-06-11 10:19:31 +0200

    feat: Bump to v26.05 | Last push to this repo ever
    My work configuration now contaiins some (could be) sensible data on
    which I am not comfortable sharing.
    
    This will sadly be tha last commit to this public repo, after which I
    will archive it. It has been a fun ride. See you

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixos-wsl = {
         url = "github:nix-community/NixOS-WSL/main";
         inputs.nixpkgs.follows = "nixpkgs";
